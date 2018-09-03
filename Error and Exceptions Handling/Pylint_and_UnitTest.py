384f7631b125591f3b1db933dc57abf04c613a4e
Mulani <Ashpak_Mulani@Dell.com>
2018-09-03 19:06:04 +0530

    Python learning

---
+++ Error and Exceptions Handling/Pylint_and_UnitTest.py
@@ -0,0 +1,18 @@
+
+###########################################################################################
+##############################    Pylint    ###############################################
+# pip install pylint
+# Pylint : It is a code linter which analyzes python code and provide feedback and quality scrore for our code
+# It validate against PEP8 standards and help us witting better code. It validate examples like
+#Missing DOCSTRINGS for function and class
+#class name is invalid if not used camelcase for naming
+#constant name should be in capitals
+# unused veriables
+#tabs and spaces are not used properly
+
+# use >>> pylint <path of python file to evaluate>
+
+
+###########################################################################################
+
+
